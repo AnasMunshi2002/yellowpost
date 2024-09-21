5e79df826bb779171ebd73fc03dab68be055abab
AnasMunshi <anasmunshi2002@gmail.com>
2024-09-21 15:55:14 +0530

    read functionality added

--- lib/model class/visitedList.dart
+++
@@ -1,5 +0,0 @@
-import 'package:yellowpost/model%20class/Post.dart';
-
-class Visitedlist {
-  static List<Post> visitedList = [];
-}
--- lib/screens/postscreen.dart
+++ lib/screens/postscreen.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:yellowpost/localdb/dbservice.dart';
 import 'package:yellowpost/screens/buildercomponent.dart';
-import 'package:yellowpost/screens/hivebuildercomponent.dart';
-import 'package:yellowpost/service/httpService.dart';
 
 class PostScreen extends StatefulWidget {
   const PostScreen({super.key});
@@ -14,21 +10,6 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
-  final _mypostBox = Hive.box('posts');
-  DBService dbService = DBService();
-  HttpService httpService = HttpService();
-  List hivePostList = [];
-  @override
-  void initState() {
-    super.initState();
-    fetchHive();
-  }
-
-  fetchHive() async {
-    hivePostList = _mypostBox.get('postsDB');
-    print(hivePostList);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +21,9 @@ class _PostScreenState extends State<PostScreen> {
         ),
       ),
       body: Container(
-          height: double.infinity, width: double.infinity, child: MyBuilder()),
+          height: double.infinity,
+          width: double.infinity,
+          child: const MyBuilder()),
     );
   }
 }
